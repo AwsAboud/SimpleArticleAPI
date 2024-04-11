115b8d0640602866d120b96e4c10071ea6f47d53
AwsAboud <aws.amin.aboud@gmail.com>
2024-04-11 18:33:00 +0300

    Setup  Migrations and Models

--- database/migrations/2024_04_11_144520_create_authors_table.php
+++ database/migrations/2024_04_11_144520_create_authors_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('authors', function (Blueprint $table) {
             $table->id();
-            $table->boolean('is_admin');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('country');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('authors');
     }
 };

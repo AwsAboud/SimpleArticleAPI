9a1b2148b14ca6d5319e99998acd4abcc2a49cb6
AwsAboud <aws.amin.aboud@gmail.com>
2024-04-12 12:12:36 +0300

    Author Managment

---
+++ app/Http/Controllers/Api/V1/AuthorController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreAuthorRequest;
+use App\Http\Requests\UpdateAuthorRequest;
+use App\Http\Resources\AuthorResource;
+use App\Models\Author;
+use Illuminate\Http\Response;
+
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return AuthorResource::collection(Author::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAuthorRequest $request)
+    {
+        $author = Author::create($request->validated());
+        return new AuthorResource($author);
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Author $author)
+    {
+        return new AuthorResource($author);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAuthorRequest $request, Author $author)
+    {
+        $author->update($request->validated());
+        return new AuthorResource($author);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Author $author)
+    {
+        $author->delete();
+        return response()->json(null, Response::HTTP_NO_CONTENT);
+    }
+}
